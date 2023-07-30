0608c2d9f835e0376f66fefafa47558d347cf9f6
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-07-30 17:26:21 +0300

    Add content saving functionality

---
+++ src/main/java/io/github/uptalent/content/service/ContentService.java
@@ -0,0 +1,30 @@
+package io.github.uptalent.content.service;
+
+import io.github.uptalent.content.client.AccountClient;
+import io.github.uptalent.content.model.common.Author;
+import io.github.uptalent.content.model.request.ContentModify;
+import io.github.uptalent.content.model.response.ContentDetailInfo;
+import io.github.uptalent.content.service.visitor.ContentSaveVisitor;
+import io.github.uptalent.content.service.visitor.ContentUpdateVisitor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+
+@Service
+@RequiredArgsConstructor
+public class ContentService {
+    private final AccountClient accountClient;
+    private final ContentSaveVisitor contentSaveVisitor;
+    private final ContentUpdateVisitor contentUpdateVisitor;
+
+
+    public URI saveContent(ContentModify contentModify) {
+        Author author = accountClient.getAuthor();
+        return contentModify.accept(author, contentSaveVisitor);
+    }
+
+    public ContentDetailInfo updateContent(String userId, String contentId, ContentModify contentModify) {
+        return contentModify.accept(userId, contentId, contentUpdateVisitor);
+    }
+}
