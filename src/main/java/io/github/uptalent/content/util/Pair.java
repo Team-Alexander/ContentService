70f61ca76fbdf092b63986933270d1bf769be780
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-07-30 17:27:15 +0300

    Add Pair utility class in content service

---
+++ src/main/java/io/github/uptalent/content/util/Pair.java
@@ -0,0 +1,23 @@
+package io.github.uptalent.content.util;
+
+public class Pair<S, T> {
+    private final S first;
+    private final T second;
+
+    public Pair(S first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pair<?, ?> pair)) return false;
+        return first.equals(pair.first) && second.equals(pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * first.hashCode() + second.hashCode();
+    }
+}
