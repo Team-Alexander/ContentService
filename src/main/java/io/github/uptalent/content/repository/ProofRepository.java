e7d6dc1e0753590469e3e60eb8c666d131d538dc
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-08-01 17:45:19 +0300

    Add methods to filter and delete by author in ProofRepository

--- src/main/java/io/github/uptalent/content/repository/ProofRepository.java
+++ src/main/java/io/github/uptalent/content/repository/ProofRepository.java
@@ -3,5 +3,10 @@ package io.github.uptalent.content.repository;
 import io.github.uptalent.content.model.document.Proof;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ProofRepository extends MongoRepository<Proof, String> {
+    List<Proof> findAllByAuthorId(String authorId);
+
+    void deleteAllByAuthorId(String authorId);
 }
