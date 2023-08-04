bb13b16b147949e3172629aa0d762667dfd1b46f
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-08-04 15:00:57 +0300

    feat: create document KudosHistory and related repository

---
+++ src/main/java/io/github/uptalent/content/model/document/KudosHistory.java
@@ -0,0 +1,24 @@
+package io.github.uptalent.content.model.document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document("kudos_histories")
+public class KudosHistory {
+    private String id;
+    private String proofId;
+    private long sponsorId;
+    private long kudos;
+    private LocalDateTime sent;
+    private List<SkillKudos> kudosedSkills;
+}
