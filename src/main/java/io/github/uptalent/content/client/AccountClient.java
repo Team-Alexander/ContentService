3195350346c03fc23615cf2a5b3b2f7d5169c660
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-08-04 15:05:48 +0300

    feat: create feign client requests for working with sponsor balance

--- src/main/java/io/github/uptalent/content/client/AccountClient.java
+++ src/main/java/io/github/uptalent/content/client/AccountClient.java
@@ -4,10 +4,18 @@ import io.github.uptalent.content.model.common.Author;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient("account-service")
 @Retry(name = "default")
 public interface AccountClient {
     @GetMapping("/api/v1/account/author")
     Author getAuthor();
+
+    @GetMapping("/api/v1/account/sponsors/balance")
+    Long getSponsorBalance();
+
+    @PatchMapping("/api/v1/account/sponsors/balance")
+    void updateSponsorBalance(@RequestBody Long balance);
 }
