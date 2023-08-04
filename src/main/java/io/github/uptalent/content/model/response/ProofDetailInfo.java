6115d361404a435ef5bbd2ece901e215e91aff31
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-08-04 20:03:21 +0300

    Add skills map to ProofDetailInfo model

--- src/main/java/io/github/uptalent/content/model/response/ProofDetailInfo.java
+++ src/main/java/io/github/uptalent/content/model/response/ProofDetailInfo.java
@@ -4,11 +4,14 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
 
+import java.util.Map;
+
 @Data
 @SuperBuilder
 @EqualsAndHashCode(callSuper = true)
 public class ProofDetailInfo extends ContentDetailInfo {
     private int iconNumber;
     private String summary;
+    private Map<String, Long> skills;
     private long kudos;
 }
