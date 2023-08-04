0152178bb01e85b9a315c11b5faf33b6e435da62
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-08-04 14:49:42 +0300

    feat: rename ContentSaveVisitorImpl to ProofSaveVisitor

--- src/main/java/io/github/uptalent/content/service/visitor/ProofSaveVisitor.java
+++ src/main/java/io/github/uptalent/content/service/visitor/ProofSaveVisitor.java
@@ -1,28 +1,36 @@
 package io.github.uptalent.content.service.visitor;
 
+import io.github.uptalent.content.exception.IllegalContentModifyingException;
 import io.github.uptalent.content.mapper.ProofMapper;
 import io.github.uptalent.content.model.common.Author;
 import io.github.uptalent.content.model.document.Proof;
 import io.github.uptalent.content.model.request.ProofModify;
-import io.github.uptalent.content.repository.ProofRepository;
 import io.github.uptalent.content.service.ProofService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import static io.github.uptalent.content.model.enums.ContentStatus.PUBLISHED;
 
 @Service
 @RequiredArgsConstructor
-public class ContentSaveVisitorImpl implements ContentSaveVisitor {
+public class ProofSaveVisitor implements ContentSaveVisitor {
     private final ProofService proofService;
     private final ProofMapper proofMapper;
 
     @Override
     public URI saveContent(Author author, ProofModify proofModify) {
+        if (proofModify.getSkills().isEmpty() && proofModify.getStatus().equals(PUBLISHED)) {
+            throw new IllegalContentModifyingException("Skills should be set for publishing");
+        }
+
         Proof proof = proofMapper.toProof(proofModify);
         proof.setAuthor(author);
+        proof.setSkills(proofMapper.convertSkills(proofModify.getSkills()));
         proof = proofService.save(proof);
+
+
         return ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{id}")
