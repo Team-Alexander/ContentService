841f9bf583f19291696df032c58b156771970292
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-08-05 15:34:57 +0300

    feat: create document Vacancy and his repository

---
+++ src/main/java/io/github/uptalent/content/model/document/Vacancy.java
@@ -0,0 +1,20 @@
+package io.github.uptalent.content.model.document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Set;
+
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document("vacancies")
+@EqualsAndHashCode(callSuper = true)
+public class Vacancy extends Content {
+    private Set<String> skills;
+}
