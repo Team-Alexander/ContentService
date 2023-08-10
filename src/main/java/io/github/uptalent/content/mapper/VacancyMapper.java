10cffb9b8cf5c12f0d519bcd8903bb3ce582eec5
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-08-10 16:15:53 +0300

    Add new mapping methods in VacancyMapper

--- src/main/java/io/github/uptalent/content/mapper/VacancyMapper.java
+++ src/main/java/io/github/uptalent/content/mapper/VacancyMapper.java
@@ -1,10 +1,12 @@
 package io.github.uptalent.content.mapper;
 
+import io.github.uptalent.content.model.document.Submission;
 import io.github.uptalent.content.model.document.Vacancy;
+import io.github.uptalent.content.model.request.SubmissionRequest;
 import io.github.uptalent.content.model.request.VacancyModify;
-import io.github.uptalent.content.model.response.VacancyDetailInfo;
-import io.github.uptalent.content.model.response.VacancyGeneralInfo;
+import io.github.uptalent.content.model.response.*;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -13,4 +15,10 @@ public interface VacancyMapper {
     Vacancy toVacancy(VacancyModify vacancyModify);
     List<VacancyGeneralInfo> toVacancyGeneralInfoList(List<Vacancy> vacancies);
     VacancyDetailInfo toVacancyDetailInfo(Vacancy vacancy);
+    TalentVacancyDetailInfo toTalentVacancyDetailInfo(Vacancy vacancy);
+    SponsorVacancyDetailInfo toSponsorVacancyDetailInfo(Vacancy vacancy);
+    Submission toSubmission(SubmissionRequest submissionRequest);
+    SubmissionGeneralInfo  toSubmissionGeneralInfo(Submission submission);
+    SubmissionDetailInfo toSubmissionDetailInfo(Submission submission);
+    ContentGeneralInfo  toContentGeneralInfo(Vacancy vacancy);
 }
